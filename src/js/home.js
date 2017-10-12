2dc9222c5f10044efbf1ed35e736b80595270577
Miguel Boanova <mgl.deadly@gmail.com>
2017-10-12 20:30:03 -0300

    :sparkles: adicionado animação ao abrir e fechar menu

--- src/js/home.js
+++ src/js/home.js
@@ -1,11 +1,28 @@
 class Home {
     constructor() {
-        console.log('calledHESAU');
+        //console.log('calledHESAU');
+        this.menu = false;
+        this.$menu = document.querySelector('nav');
+        this.$hamburguer = document.querySelector('.mobile-nav-button');
     }
 
     init() {
-        console.log(this);
+        console.log(this.menu);
+    }
+
+    toogleMenu() {
+        this.menu = !this.menu;
+        if (this.menu) {
+            this.$menu.setAttribute('class', 'active');
+        } else {
+            this.$menu.removeAttribute('class');
+            this.$menu.setAttribute('class', 'desactived');
+        }
+        console.log("Menu ativo?" , this.menu);
     }
 }
-
+let home = new Home();
+home.$hamburguer.addEventListener('click', function() {
+    home.toogleMenu();
+});
 export default new Home();
